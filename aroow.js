cf4ef8b3575082ef2bfc1e3b5acd96f0b25859b0
sharoj57 <sharojcse@gmail.com>
2024-09-27 23:24:11 +0600

    added this part

--- aroow.js
+++ aroow.js
@@ -19,9 +19,37 @@
 
 
 // --Function Express--//
-const add2 = function (a,b) {
-    return a + b;
-}
+// const add2 = function (a,b) {
+//     return a + b;
+// }
 
-const final = add2(6,6);
-console.log(final);
+// const final = add2(6,6);
+// console.log(final);
+
+// // ---Arrow Function---Multiple Parameter//
+// const difference = (x,y) => x-y;
+// const result = difference (6,70);
+// console.log (result);
+
+// const sum = (x,y,z,a) => x+y+z+a;
+// const sumOfNumber = sum(5,6,10,90);
+// console.log (sumOfNumber);
+
+//--Arrow Function Single Parameter--//
+// const getAge = (person) => person.age;
+// const student = { name: 'Shahadat', age: 70};
+// const age = getAge(student);
+// console.log(age);
+
+// const getAge = (person) => person.age;
+// const student = {name : 'Shahadat', age: 80};
+// const age = getAge(student);
+// console.log(age); 
+
+// const getThird = (number) => number [4];
+// const third = getThird ([65,83,94,23,67,874]);
+// console.log (third);
+
+// ---- No Parameter----//
+const getPi = () => Math.PI;
+console.log(getPi());
