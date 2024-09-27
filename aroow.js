1457c3839530d9ceb412a664a57f333a4a5a73d2
sharoj57 <sharojcse@gmail.com>
2024-09-27 12:35:05 +0600

    added this part

---
+++ aroow.js
@@ -0,0 +1,27 @@
+
+// --Function Declaration--Method One--//
+
+// function add (a,b){
+//     const result = a+b;
+//     return result;
+// }
+// const sum = add(50,60);
+// console.log (sum);
+
+
+// --Function Declaration--Method Two--//
+
+// function add (a,b,c,d){
+//     return a+b+c+d;
+// }
+// const result = add(5,5,5,5)
+// console.log(result);
+
+
+// --Function Express--//
+const add2 = function (a,b) {
+    return a + b;
+}
+
+const final = add2(6,6);
+console.log(final);
