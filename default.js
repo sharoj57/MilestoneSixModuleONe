546e1996bb8f5aa7db17719eed9f56ecd0fcc6e3
sharoj57 <sharojcse@gmail.com>
2024-09-27 10:56:43 +0600

    aded the template.js file

--- default.js
+++ default.js
@@ -1,7 +1,19 @@
-function add(num1, num2) {
-    const result = num1+num2;
-    console.log(num1,num2,result);
+// function add(num1, num2) {
+//     const result = num1+num2;
+//     console.log(num1,num2,result);
+//     return result;
+// }
+
+// const sum = add(5,7);
+function add(num1, num2, num3){
+    const result = num1+ num2 + num3;
+    console.log(num1,num2,num3,result);
     return result;
 }
+// //  const sum= add(12,65,85);
+// //  Zodi Ekhane 3 tar bodole 2 ta man dei tahole output ase (12 65 undefined NaN)
+//  const sum= add(12,65);
 
-const sum = add(5,7);
+// Zodi man ektao na dei tahole
+ const sum= add();
+
---
+++ template.js
@@ -0,0 +1,24 @@
+// const a = 10;
+// const b = 30;
+// const sum = 'The Sum Of ' +a+ ' and ' +b+ ' is ' + (a+b);
+// console.log(sum);
+
+
+// const result = `The Sum of ${a} and ${b} is ${a+b}`;
+// console.log (result);
+
+// const secondresult = `The Minus of ${a} and ${b} is ${a-b}`;
+// console.log (secondresult);
+
+// const multipy = `The Multyply of ${a} and ${b} is ${a*b}`;
+// console.log(multipy);
+
+// const numbers = [56, 76, 89, 100];
+// const students = {name : 'Shahadat', age : 50};
+// const result =`The Sum of ${numbers[3]} and ${students.age} is ${numbers[3]+students.age}`
+// console.log (result);
+
+const dhorlamektarray = [40, 50, 60, 70,80, 90, 100];
+const dhorlamektaNam = { name: 'Shona-Pakhi', age : 100};
+const duidareChaippaDhorlam = `Je Duida Chaippa Dhorchi Oi Dar ekta hoilo ${dhorlamektarray[4]} arekta hoilo ${dhorlamektaNam.age} ekhon duidar jogfol hoilo ${dhorlamektarray[4]+dhorlamektaNam.age}`;
+console.log(duidareChaippaDhorlam);
